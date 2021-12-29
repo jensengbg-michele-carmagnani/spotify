dc8a4edc84d906212f0b1b529eba0d8971077bd4
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-12-29 21:41:25 +0100

    working on sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,32 @@ const Sidebar = () => {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div>
-      
+      <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t--[0.1px] border-gray-900"/>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HomeIcon className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <SearchIcon className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t--[0.1px] border-gray-900"/>
       </div>
     </div>
   );
