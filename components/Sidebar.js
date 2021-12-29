f9269344f29118c8eb12fdb7e3a27da2f85c06db
Michele Carmagnani <michele.carmagnani@gmail.com>
2021-12-29 21:40:48 +0100

    setting up the project

---
+++ components/Sidebar.js
@@ -0,0 +1,18 @@
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+} from '@heroicons/react/outline';
+
+const Sidebar = () => {
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
